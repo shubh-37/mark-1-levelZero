da6b83d3328d6d659835f94d1535697cae4a10c7
shubh arya <74173755+shubh-37@users.noreply.github.com>
2022-09-22 06:17:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+var readlineSync = require('readline-sync');
+
+var score = 0;
+var userAnswer = readlineSync.question('What is your name? ')
+
+console.log('Welcome! '+ userAnswer + " to DO YOU KNOW MARVEL?");
+
+function play(question, answer){
+  var userAns = readlineSync.question(question);
+  
+  if(userAns == answer){
+  console.log('right!!')
+  score = score+1;
+  }else{
+  console.log('oops!! wrong')
+  }
+}
+
+var quesArray = [
+  {
+    question: "How many infinity stones are there? ",
+    answer: 6,
+  },
+  {
+    question: "What is the name of thor's second hammer? ",
+    answer: "stormbreaker",
+  },
+  {
+    question: "What is the name of guider to soul stone? ",
+    answer: "redskull"
+  }
+]
+
+for (let i = 0; i < quesArray.length; i++){
+  currentQues = quesArray[i];
+  console.log(play(currentQues.question, currentQues.answer))
+}
+
+console.log("YOU SCORED " + score + "!!!");
+
